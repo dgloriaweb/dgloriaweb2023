5e340ebfd3bb6233ed84d8b92e87da8d5f97a544
unknown <dgloria.web@gmail.com>
2024-12-10 07:00:55 +0000

    project ideas page

---
+++ src/views/IdeaView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container" id="app">
+        <div>
+            <h1>Project ideas</h1>
+        </div>
+        <div>
+            <h2>Council housing management</h2>
+            <p>This project is about collecting data about real estates that are in the handling of local councils. </p>
+            <h3>Az ingatlanokkal kapcsolatos problémák:</h3>
+            <ul>
+                <li>Tulajdonos</li>
+                <li>Ingatlan állapota</li>
+                <li>Ingatlan kiadása rászorulók részére</li>
+                <li>Állapot monitorozása</li>
+                <li>Bejelentések kezelése</li>
+            </ul>
+            <h3>A fentiekhez kapcsolódó feladatok</h3>
+            <ul>
+                <li>Fenntartó managelése</li>
+                <ul>
+                    <li>Tableten futó appon mutatni napi feladatokat</li>
+                    <li>Óra elszámolás</li>
+                </ul>
+                <li>Bérlőktől pályázatok befogadása és elbírálása</li>
+                <ul>
+                    <li>Bankszámla kapcsolat</li>
+                    <li>szociális háttér vizsgálata</li>
+                </ul>
+                <li>Pénzügyek managelése</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
